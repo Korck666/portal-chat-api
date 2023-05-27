86d313d0e3968b87cd75b8dbfd7354667541cfdf
Rafael Aguilher da Costa <rafael.aguilher@gmail.com>
2023-05-27 11:21:41 +0000

    Changes to be committed: 	modified:   .devcontainer/devcontainer.json 	modified:   .vscode/settings.json 	modified:   app/scripts/postcommand.sh
    Configuring development environment

--- app/scripts/postcommand.sh
+++ app/scripts/postcommand.sh
@@ -8,7 +8,7 @@ apt-get update
 apt-get install -y --no-install-recommends apt-utils curl wget vim coreutils zsh
 
 # Download and run Oh My Zsh install script
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Change Oh My Zsh theme
 sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
